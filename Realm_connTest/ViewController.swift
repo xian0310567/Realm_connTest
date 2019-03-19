7b435cbd055a1d4243a0a4fc4a1860d015db12a5
sangjoon <gofflvhxj12@naver.com>
2019-03-19 11:24:47 +0900

    Initial Commit

---
+++ Realm_connTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Realm_connTest
+//
+//  Created by dit on 2019. 3. 19..
+//  Copyright © 2019년 dit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
