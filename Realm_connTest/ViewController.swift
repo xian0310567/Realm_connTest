7523f7f4253f2669207856c867a67dce4ad17429
sangjoon <gofflvhxj12@naver.com>
2019-03-19 13:33:26 +0900

    Adjust to Remark

--- Realm_connTest/ViewController.swift
+++ Realm_connTest/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import Realm
+import RealmSwift
+
+class Todo: Object {
+    @objc dynamic var title : String =  ""
+    @objc dynamic var dueDate : Date!
+}
 
 class ViewController: UIViewController {
+    
+    func addTodo(title: String, dueDate: Date) {
+        let todo = Todo()
+        todo.title = title
+        todo.dueDate = dueDate
+        
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(todo)
+        }
+//            실행. realm.write
+        print("DB연결성공")
+//        이번은 한번에 4개의 DB를 생성하니까 총 4번 실행
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        addTodo(title: "todo1", dueDate: Date())
+        addTodo(title: "todo2", dueDate: Date())
+        addTodo(title: "todo3", dueDate: Date())
+        addTodo(title: "todo4", dueDate: Date())
+        
+        print(NSHomeDirectory())
+//        Realm 파일의 경로를 알기위해 print로 출력해줌
     }
 
     override func didReceiveMemoryWarning() {
